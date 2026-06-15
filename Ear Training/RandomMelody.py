87d1ee4b173488c6ba6118829dd58dcf46bbe1ab
therealnickg <75397239+therealnickg@users.noreply.github.com>
2026-06-15 16:35:26 -0700

    Latest version of files

--- Ear Training/RandomMelody.py
+++ Ear Training/RandomMelody.py
@@ -1,13 +1,37 @@
-# Import required modules
+# Import REQUIRED modules and libraries
 import secrets # To use random generator for numbers
 from playsound import playsound # To play sounds on computer
 from playlist import Playlist # To import playlist array file
 from TempWAV import TempWAVfile # To import custom WAV transposer
 
-# Did the user input a valid string on the guitar?
-# Must be a character that is a number from 0 to 6 to be true
+# Find the maximum note the user can play on their instrument
+# Minimum note is obviously assumed to be zero (0)
+# Maximum note is always 24 notes higher than the number of frets (2 octaves)
+def findMaxPlayableNote(numFrets):
+	return 24 + numFrets
+
+# Checks if user input is a valid mode of operation from playlist file
+# Must be a number character from 0 to 4 to return True
+# Empty string mode may also be accepted (changes to default 1 later)
+def isValidMode(mode):
+	if mode == "0":
+		return True
+	if mode == "1" or mode == "":
+		return True
+	if mode == "2":
+		return True
+	if mode == "3":
+		return True
+	if mode == "4":
+		return True
+	return False
+
+# Checks if user input is a valid string on the guitar
+# Must be a number character from 0 to 6 to return True
+# Selecting 0 will choose random string later
+# Empty user input may also be accepted (changes to default 0 later)
 def isValidGuitarString(guitarStr):
-	if guitarStr == "0":
+	if guitarStr == "0" or guitarStr == "":
 		return True
 	if guitarStr == "1":
 		return True
@@ -39,49 +63,65 @@ def createReferenceToneNumber(guitarStr, numFrets):
 		return secrets.randbelow(numFrets + 1) + 10
 	if guitarStr == "5":
 		return secrets.randbelow(numFrets + 1) + 5
-	return secrets.randbelow(numFrets + 1)
-
-# Find the maximum note the user can play on their instrument
-# Minimum note is obviously assumed to be zero (0)
-# Maximum note is always 24 notes higher than the number of frets (2 octaves)
-def findMaxPlayableNote(numFrets):
-	return 24 + numFrets
+	return secrets.randbelow(numFrets + 1) # Base case 6th string
 
 # Introduction
 print("\n\nHello, this is the Ear to Instrument Trainer!")
-print("This is the random sound practice with the ability to choose which string.")
+print("This is the random sound practice with the ability to select a string.")
 
-userInput = "N" # Sets user up to start the exercise
+userInput = "N" # DEFAULT "N" sets user up to start the exercise
 numberOfFrets = 15 # Number of playable frets on your guitar
 # Classical Guitar default is 15
 # Acoustic Guitar default is 20
 # Electric Guitar default is 22
 maxNote = findMaxPlayableNote(numberOfFrets) # Find the maximum playable note on the instrument
 
+# User selects the mode of operation
+print("\nModes of Operation:")
+print("\t0. On-demand sounds (e.g. weak areas or debugging)")
+print("\t1. All the sounds (DEFAULT press ENTER)")
+print("\t2. Scales (Major, Minor, Pentatonic, Harmonic, Modes, etc.)")
+print("\t3. Triads, Chords, and Arpeggios (7ths, 9ths, Diminished, etc.)")
+print("\t4. Licks and Melodies")
+print("What mode of operation would you like to select?\n")
+modeOfOps = input("Enter a number from 0 to 4: ")
+while (not isValidMode(modeOfOps)):
+	print("That was not a valid number from 0 to 4.")
+	modeOfOps = input("Enter a number from 0 to 4: ")
+# If the mode is an empty string, it will changed to the default mode 1
+if (modeOfOps == ""):
+	modeOfOps = "1"
+
 # User picks string and random note on string (reference tone) is selected
-print("\nWhat guitar string would you like to use for this session?")
-print("Choose a number from 1 to 6. Enter 0 for a random string.\n")
+print("\n\n\nWhat guitar string would you like to use for this session?")
+print("Choose a number from 1 to 6. Choose 0 (or press ENTER) for random string.\n")
 referenceStringInput = input("Enter a number from 0 to 6: ")
 while (not isValidGuitarString(referenceStringInput)):
 	print("That was not a valid number from 0 to 6.")
 	referenceStringInput = input("Enter a number from 0 to 6: ")
+# If the input is an empty string, it will changed to the default mode 0
+if (referenceStringInput == ""):
+	referenceStringInput = "0"
 # If a random string is selected (0), then automatically choose one for the user
 if (referenceStringInput == "0"):
 	randoString = secrets.randbelow(6) + 1
-	print("Randomly chosen string: " + str(randoString))
 	referenceStringInput = str(randoString)
+	print("\nRandomly chosen string: " + str(referenceStringInput))
 
 # Generates random reference tone for exercise
 referenceTone = createReferenceToneNumber(referenceStringInput, numberOfFrets)
 
-# Generates an empty playlist and adds sounds based on reference tone 
-pList = Playlist(referenceTone, maxNote)
+# Generates an empty playlist (using Playlist class)
+# Adds sounds based on reference tone 
+pList = Playlist(modeOfOps, referenceTone, maxNote)
 sound = pList.getNextSound()
-TempWAVfile(sound)
+TempWAVfile(sound) # Is this one required? I'm not sure since 1st node is dummy
 count = 0 # Counter for how many sounds have been played
 
 print("\nN for new sound, R to repeat interval, T to reset reference tone.")
-print("Nothing to exit.\n")
+print("Nothing to exit.")
+
+# FLOW LOGIC FOR THE PROGRAM BELOW
 
 while userInput != "":
 
@@ -97,9 +137,10 @@ while userInput != "":
 
 		if len(pList.playlist) > 0:
 			sound = pList.getNextSound()
-			TempWAVfile(sound)
+			TempWAVfile(sound) # Generates WAV file of sound
 			# Print current count of sounds played and remaining sounds in bank
-			print("Count: " + str(count) + ", Remaining: " + str(pList.lengthPlaylist()))
+			print("\nCount: " + str(count) + ", Remaining: " + str(pList.lengthPlaylist()))
+			print("Current playing: ID_" + sound[0] + "\n")
 
 			playsound("Sounds/Notes/"+str(referenceTone)+'.wav') # Play original reference tone
 			playsound('Sounds/Temp.wav') # Play mystery sound
--- Ear Training/playlist.py
+++ Ear Training/playlist.py
@@ -5,18 +5,19 @@ class Playlist:
 	# Create noteNumber and playlist members
 	# noteNumber is the reference tone
 	# maxNote is the maximum playable note on the instrument
-	def __init__(self, noteNumber, maxNote):
+	def __init__(self, modeOfOps, noteNumber, maxNote):
 		self.noteNumber = noteNumber
 		self.maxNote = maxNote
 		self.playlist = []
 		self.soundSet = set()
-		self.addToSoundSet(0) # This chooses mode of operation
+		self.addToSoundSet(modeOfOps) # Chooses mode of operation, DEFAULT = 1
 		self.addStrings()
 		self.randomize()
-		self.playlist.append(['0', 0, 0, 3]) # DUMMY NODE (Values not important)
+		# DUMMY NODE is added to the front of the list
+		self.playlist.append(['0', 0, 0, 3]) # (VALUES NOT IMPORTANT)
 
 	# Get the last element in the list by popping it
-	# The noteNumber is appended to the list before returning it
+	# The referenceNote number is appended to the list before returning it
 	# This allows the ability to change reference tone
 	def getNextSound(self):
 		sound = self.playlist.pop()
@@ -56,29 +57,36 @@ class Playlist:
 	# Do NOT confuse these "modes" with musical modes! These are only modes of operation
 	# This allows for different modes (licks, triads, scales, etc.)
 	def addToSoundSet(self, mode):
-		# Mode 0: Reserved for on-demand sounds (such as to pinpoint weak areas)
-		if (mode == 0):
-			self.soundSet.update({0})
-		# Mode 1: All the sounds
-		if (mode == 1):
-			self.soundSet.update({0, 1, 2, 3, 4, 9, 11, 12, 13})
+		# soundSet is empty by default
+
+		# Mode 0: Reserved for on-demand sounds (e.g. pinpoint weak areas or debugging)
+		if (mode == "0"):
+			self.soundSet.update({16})
+		# Mode 1: ALL THE SOUNDS
+		if (mode == "1"):
+			# Adds all the sounds with a for loop but then exludes certain sounds
+			for i in range(21):
+				self.soundSet.update({i})
+			self.soundSet.difference_update({})
 		# Mode 2: Scales (Major, Minor, Pentatonic, Harmonic, Modes, etc.)
-		if (mode == 2):
-			self.soundSet.update({0, 3, 5, 6, 11, 12, 13})
-		# Mode 3: Triads, Chords, Arpeggios (including 7ths, 9ths, Diminished, Half-Diminished)
-		if (mode == 3):
-			self.soundSet.update({4, 8})
+		if (mode == "2"):
+			self.soundSet.update({0, 3, 5, 6, 11, 12, 13, 14, 15})
+		# Mode 3: Triads, Chords, and Arpeggios (7ths, 9ths, Diminished, Half-Diminished, etc.)
+		if (mode == "3"):
+			self.soundSet.update({4, 8, 16, 18, 19, 20, 21})
 		# Mode 4: Licks and Melodies
-		if (mode == 4):
-			self.soundSet.update({1, 2, 7, 9, 10})
-		# Do we want to exclude any sounds? Sounds in the set below will be exluded.
+		if (mode == "4"):
+			self.soundSet.update({1, 2, 7, 9, 10, 17})
+
+		# Sounds in the excluded set will NOT be included in the exercise
 		# Set should be empty by default
 		self.soundSet.difference_update({})
 
+	# Add sounds to playlist from their corresponding number
+	# A sound must be contained in hashset in order to be added (depends on mode)
+	# List format is as follows:
+	# [MIDI_FILE_NAME, CORRECTION_CONSTANT, POSITION, TRIM_DURATION_MS]
 
-	# Add strings containing name to playlist based on reference note
-	# A sound must be contained in hashset in order to be added
-	# List Format is [MIDI_FILE_NAME, CORRECTION, TRIM_DURATION_MS]
 	# UNFORTUNATELY PRETTY MUCH HARDCODED LOGIC :(((
 	def addStrings(self):
 
@@ -89,21 +97,44 @@ class Playlist:
 		if self.noteNumber >= 12:
 			if 0 in self.soundSet: self.playlist.append(['0', 0, -12, 5000])
 			if 3 in self.soundSet: self.playlist.append(['3', 0, -12, 5600])
+			if 5 in self.soundSet: self.playlist.append(['5', 0, -12, 5600])
+			if 6 in self.soundSet: self.playlist.append(['6', 0, -12, 5600])
 			if 12 in self.soundSet: self.playlist.append(['12', 0, -12, 5000])
-			if 11 in self.soundSet: self.playlist.append(['11', 0, -12, 5000])
+			if 11 in self.soundSet: self.playlist.append(['11', 0, -12, 5600])
 			if 13 in self.soundSet: self.playlist.append(['13', 0, -12, 5600])
+			if 14 in self.soundSet: self.playlist.append(['14', 0, -12, 5600])
+			if 15 in self.soundSet: self.playlist.append(['15', 0, -12, 5600])
 
 		if self.noteNumber >= 9:
 			if 2 in self.soundSet: self.playlist.append(['2', -9, 0, 3400]) # REFERENCE 2
+			if 16 in self.soundSet: self.playlist.append(['16', 0, -9, 2700])
+			if 19 in self.soundSet: self.playlist.append(['19', 0, -9, 2700])
+
+		if self.noteNumber >= 8:
+			if 8 in self.soundSet: self.playlist.append(['8', 0, -8, 2700])
+			if 20 in self.soundSet: self.playlist.append(['20', 0, -8, 2700])
 
 		if self.noteNumber >= 7:
 			if 4 in self.soundSet: self.playlist.append(['4', 0, -7, 2700])
+			if 18 in self.soundSet: self.playlist.append(['18', 0, -7, 2700])
+
+		if self.noteNumber >= 5:
+			if self.noteNumber <= self.maxNote - 3:
+				if 20 in self.soundSet: self.playlist.append(['20', 0, -5, 2700])
+			if self.noteNumber <= self.maxNote - 4:
+				if 16 in self.soundSet: self.playlist.append(['16', 0, -5, 2700])
 
 		if self.noteNumber >= 4:
+			if self.noteNumber <= self.maxNote - 5:
+				if 19 in self.soundSet: self.playlist.append(['19', 0, -4, 2700])
 			if 1 in self.soundSet: self.playlist.append(['1', 0, -4, 2700])
 			if 4 in self.soundSet: self.playlist.append(['4', 0, -4, 2700])
 
 		if self.noteNumber >= 3:
+			if self.noteNumber <= self.maxNote - 4:
+				if 18 in self.soundSet: self.playlist.append(['18', 0, -3, 2700])
+			if self.noteNumber <= self.maxNote - 5:
+				if 8 in self.soundSet: self.playlist.append(['8', 0, -3, 2700])
 			if self.noteNumber <= self.maxNote - 9:
 				if 0 in self.soundSet: self.playlist.append(['0', 0, -3, 5000])
 
@@ -111,7 +142,7 @@ class Playlist:
 			if self.noteNumber <= self.maxNote - 7:
 				if 2 in self.soundSet: self.playlist.append(['2', -9, 7, 3400])
 
-		# 
+		# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 
 		if self.noteNumber <= self.maxNote - 4:
 			if 1 in self.soundSet: self.playlist.append(['1', 0, 0, 2700]) # REFERENCE 1
@@ -119,16 +150,27 @@ class Playlist:
 		if self.noteNumber <= self.maxNote - 7:
 			if 4 in self.soundSet: self.playlist.append(['4', 0, 0, 2700]) # REFERENCE 4
 			if 9 in self.soundSet: self.playlist.append(['9', 0, 0, 3400]) # REFERENCE 9
+			if 18 in self.soundSet: self.playlist.append(['18', 0, 0, 2700]) # REFERENCE 18
 
 		if self.noteNumber <= self.maxNote - 8:
 			if 1 in self.soundSet: self.playlist.append(['1', 0, 4, 2700])
+			if 8 in self.soundSet: self.playlist.append(['8', 0, 0, 2700]) # REFERENCE 8
+			if 20 in self.soundSet: self.playlist.append(['20', 0, 0, 2700]) # REFERENCE 20
+
+		if self.noteNumber <= self.maxNote - 9:
+			if 16 in self.soundSet: self.playlist.append(['16', 0, 0, 2700]) # REFERENCE 16
+			if 19 in self.soundSet: self.playlist.append(['19', 0, 0, 2700]) # REFERENCE 19
 
 		if self.noteNumber <= self.maxNote - 12:
 			if 0 in self.soundSet: self.playlist.append(['0', 0, 0, 5000]) # REFERENCE 0
 			if 3 in self.soundSet: self.playlist.append(['3', 0, 0, 5600]) # REFERENCE 3
+			if 5 in self.soundSet: self.playlist.append(['5', 0, 0, 5600]) # REFERENCE 5
+			if 6 in self.soundSet: self.playlist.append(['6', 0, 0, 5600]) # REFERENCE 6
 			if 11 in self.soundSet: self.playlist.append(['11', 0, 0, 5600]) # REFERENCE 11
 			if 12 in self.soundSet: self.playlist.append(['12', 0, 0, 5000]) # REFERENCE 12
 			if 13 in self.soundSet: self.playlist.append(['13', 0, 0, 5600]) # REFERENCE 13
+			if 14 in self.soundSet: self.playlist.append(['14', 0, 0, 5600]) # REFERENCE 14
+			if 15 in self.soundSet: self.playlist.append(['15', 0, 0, 5600]) # REFERENCE 15
 
 		if self.noteNumber <= self.maxNote - 17:
 			if 3 in self.soundSet: self.playlist.append(['3', 0, 5, 5600])
